3265bdd3f5af068c9172bf4420e3a6fe6382a23c
Artur Tenorio <Artur.Tenorio@swisscom.com>
2022-04-23 23:03:40 +0200

    add prettier

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 Simple TS project with some hackerhank  solutions with unit tests.
 
 
+## Running
+
+Test: ```npm run test```
+Nodemon: ```npm run dev``` => you must change the package.json to chose the file you want to run as main.ts
+
 ## Extras
 
 Prettier for LINT
--- src/algorithms/diagonal-difference/diagonal-difference.spec.ts
+++ src/algorithms/diagonal-difference/diagonal-difference.spec.ts
@@ -5,7 +5,7 @@ describe('Diagonal difference', () => {
     const arr = [
       [11, 2, 4],
       [4, 5, 6],
-      [10, 8, -12],
+      [10, 8, -12]
     ];
 
     expect(diagonalDifference(arr)).toBe(15);
@@ -15,7 +15,7 @@ describe('Diagonal difference', () => {
     const arr = [
       [0, 0, 0],
       [0, 0, 0],
-      [0, 0, 0],
+      [0, 0, 0]
     ];
 
     expect(diagonalDifference(arr)).toBe(0);
@@ -25,7 +25,7 @@ describe('Diagonal difference', () => {
     const arr = [
       [-1, 0, 0],
       [0, -1, 0],
-      [0, 0, -1],
+      [0, 0, -1]
     ];
 
     expect(diagonalDifference(arr)).toBe(2);
